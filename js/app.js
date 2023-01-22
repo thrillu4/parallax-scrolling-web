4bb1fac84e5628c4cd92dddb2cf905255b33a0bf
Denis <tonopik@gmail.com>
2023-01-23 00:22:01 +0200

    3 hrs of creating

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', e => {
+    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content'
+})
